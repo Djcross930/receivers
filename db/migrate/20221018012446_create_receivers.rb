c950331b7994c29ded18162353a95a7acc93053d
Dan Cross <dancross96@gmail.com>
2022-10-17 20:25:12 -0500

    added controller and models

---
+++ db/migrate/20221018012446_create_receivers.rb
@@ -0,0 +1,10 @@
+class CreateReceivers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :receivers do |t|
+      t.string :name
+      t.integer :wins
+
+      t.timestamps
+    end
+  end
+end
